869f0b17003e0e4e180e903995f875b601aa6fb7
HasutoSasaki <hasuto.sa@gmail.com>
2025-08-13 14:21:24 +0900

    feat: Implement PNG chunk reading and dumping functionality

--- pkg/reader/png_analysis.go
+++ pkg/reader/png_analysis.go
@@ -1,6 +1,57 @@
 package reader
 
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"os"
+)
+
+func dumpChunk(chunk io.Reader) {
+	var length int32
+	binary.Read(chunk, binary.BigEndian, &length)
+	buffer := make([]byte, 4)
+	chunk.Read(buffer)
+	fmt.Printf("check '%v' (%d bytes)\n", string(buffer), length)
+	if bytes.Equal(buffer, []byte("tEXt")) {
+		rawText := make([]byte, length)
+		chunk.Read(rawText)
+		fmt.Println(string(rawText))
+	}
+}
+
+func readChunks(file *os.File) []io.Reader {
+	// チャンクを格納する配列
+	var chunks []io.Reader
+
+	// 最初の８バイトを飛ばす
+	file.Seek(8, 0)
+	var offset int64 = 8
+
+	for {
+		var length int32
+		err := binary.Read(file, binary.BigEndian, &length)
+		if err == io.EOF {
+			break
+		}
+		chunks = append(chunks, io.NewSectionReader(file, offset, int64(length)+12))
+		// 次のチャンクの先頭に移動
+		// 現在位置は長さを読み終わった箇所なので
+		// チャンク名（4バイト) + データ長 + CRC(4バイト)際に移動
+		offset, _ = file.Seek(int64(length+8), 1)
+	}
+	return chunks
+}
+
 // PNGAnalysis PNG ファイルの分析処理
 func PNGAnalysis() {
-	// TODO: PNG ファイルの分析処理を実装
+	file, err := os.Open("sample/Lenna.png")
+	if err != nil {
+		panic(err)
+	}
+	chunks := readChunks(file)
+	for _, chunk := range chunks {
+		dumpChunk(chunk)
+	}
 }
